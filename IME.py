4fa204bb93fe346a7cfcabe9e45ced0bfda8833e
陈庆之 <cqz21@mails.tsinghua.edu.cn>
2023-03-20 16:46:49 +0800

    gitignore and keyerror management

--- IME.py
+++ IME.py
@@ -1,7 +1,10 @@
 import json
 import time
+from pathlib import Path
+from typing import Optional
 
 begin_mark = '^^^'
+void_mark = '???'
 
 
 def opr(filename: str, suffix: str = 'txt'):
@@ -20,16 +23,22 @@ def load_ngram_by_char(sents: list, gram_count: int, d: dict, pinyin_dict: dict)
     print("total: %d sentences" % len(sents))
     count = 0
     t = time.time()
+    if begin_mark not in d:
+        d[begin_mark] = {}
+    if void_mark not in d:
+        d[void_mark] = {}
+
     for sent in sents:
         if len(sent) == 0:
             continue
         if sent[0] in pinyin_dict:
-            if begin_mark not in d:
-                d[begin_mark] = {}
             first_syllable = pinyin_dict[sent[0]]
             if first_syllable not in d[begin_mark]:
                 d[begin_mark][first_syllable] = {}
             d[begin_mark][first_syllable][sent[0]] = d[begin_mark][first_syllable].get(sent[0], 0) + 1
+            if first_syllable not in d[void_mark]:
+                d[void_mark][first_syllable] = {}
+            d[void_mark][first_syllable][sent[0]] = d[void_mark][first_syllable].get(sent[0], 0) + 1
 
         for i in range(len(sent) - gram_count + 1):
             if not all([(char in pinyin_dict) for char in sent[i: i + gram_count]]):
@@ -40,11 +49,15 @@ def load_ngram_by_char(sents: list, gram_count: int, d: dict, pinyin_dict: dict)
             last_syllable = pinyin_dict[sent[i + gram_count - 1]]
             if last_syllable not in d[key]:
                 d[key][last_syllable] = {}
+            if last_syllable not in d[void_mark]:
+                d[void_mark][last_syllable] = {}
             d[key][last_syllable][sent[i + gram_count - 1]] = d[key][last_syllable].get(sent[i + gram_count - 1], 0) + 1
+            d[void_mark][last_syllable][sent[i + gram_count - 1]] = \
+                d[void_mark][last_syllable].get(sent[i + gram_count - 1], 0) + 1
         count += 1
         if count % 10000 == 0:
             newt = time.time()
-            print("%d sentences parsed with %d seconds" % (count, newt - t))
+            print("%d sentences parsed in %d seconds" % (count, newt - t))
 
 
 def load_char_freq(sents: list, d: dict, pinyin_dict: dict):
@@ -62,19 +75,36 @@ def parse_pinyin_by_ngram(pinyin: str, ngram_dict: dict):
     if len(syllables) == 0:
         return ''
     if len(syllables) == 1:
-        return get_max_from_dict(ngram_dict[begin_mark][syllables[0]])
-    answers = [get_max_from_dict(ngram_dict[begin_mark][syllables[0]]), ]
+        try:
+            return get_max_from_dict(ngram_dict[begin_mark][syllables[0]])
+        except KeyError:
+            try:
+                return get_max_from_dict(ngram_dict[void_mark][syllables[0]])
+            except KeyError:
+                return '一'
+    try:
+        answers = [get_max_from_dict(ngram_dict[begin_mark][syllables[0]]), ]
+    except KeyError:
+        try:
+            answers = [get_max_from_dict(ngram_dict[void_mark][syllables[0]]), ]
+        except KeyError:
+            answers = ['一', ]
     for syl in syllables[1:]:
         try:
             answers.append(get_max_from_dict(ngram_dict[answers[-1]][syl]))
         except KeyError:
-            answers.append(get_max_from_dict(ngram_dict[begin_mark][syl]))
+            try:
+                answers.append(get_max_from_dict(ngram_dict[void_mark][syl]))
+            except KeyError:
+                answers.append('一')
     return "".join(answers)
 
 
 def demo():
     with opr("2gram_sina", "json") as fi:
         ngram_dict = json.load(fi)
+    with opr("word_freq_sina", "json") as fi:
+        freq_dict = json.load(fi)
     print("loaded")
     while True:
         st = input()
@@ -82,11 +112,11 @@ def demo():
         print(parse_pinyin_by_ngram(st, ngram_dict))
 
 
-def get_ngram():
+def get_ngram(corpus_file: Path, pinyin_file: Path, output_file: Path):
     d = {}
-    with opr('/Users/casorazitora/Desktop/Learn new stuff/大二下/人智导/输入法作业/语料库/sina_news') as fi:
+    with corpus_file.open(mode='r', encoding='utf-8') as fi:
         corpus = [line.strip() for line in fi.readlines()]
-    with opr('/Users/casorazitora/Desktop/Learn new stuff/大二下/人智导/输入法作业/拼音汉字表/course_pinyin_dict', 'json') as fi:
+    with pinyin_file.open(mode='r', encoding='utf-8') as fi:
         pinyin_dict = json.load(fi)
     print("loaded!")
 
@@ -94,13 +124,13 @@ def get_ngram():
     load_ngram_by_char(corpus, 2, d, pinyin_dict)
     t1 = time.time()
     print("load 2gram: %d secs" % int(t1 - t0))
-    with opw('2gram_sina', 'json') as fo:
+    with output_file.open(mode='w', encoding='utf-8') as fo:
         json.dump(d, fo)
     t2 = time.time()
     print("write to disk: %d secs" % int(t2 - t1))
 
 
-def get_char_freq():
+def get_char_freq(corpus_file: Path, pinyin_file: Path, output_file: Path):
     d = {}
     with opr('/Users/casorazitora/Desktop/Learn new stuff/大二下/人智导/输入法作业/语料库/sina_news') as fi:
         corpus = [line.strip() for line in fi.readlines()]
@@ -117,7 +147,7 @@ def get_char_freq():
     print("write to disk: %d secs" % int(t2 - t1))
 
 
-def validate(predict_function):
+def validate(predict_function, output: Optional[Path] = None):
     with opr('validation/input') as fi:
         valid_set = [line.strip() for line in fi]
     with opr('validation/std_output') as fi:
@@ -126,9 +156,16 @@ def validate(predict_function):
     char_count = sum([len(t) for t in std_output])
     corr_char = 0
     corr_sent = 0
+    if output is not None:
+        fout = output.open(mode='w', encoding='utf-8')
+    else:
+        fout = None
+
     for i in range(len(valid_set)):
         prediction = predict_function(valid_set[i])
-        if len(prediction) != len(std_output):
+        if fout is not None:
+            print(prediction, file=fout)
+        if len(prediction) != len(std_output[i]):
             continue
         flag = True
         for j in range(len(prediction)):
@@ -139,10 +176,16 @@ def validate(predict_function):
         if flag:
             corr_sent += 1
 
+    if fout is not None:
+        fout.close()
     print("By character: %d / %d, accuracy %.4f" % (corr_char, char_count, corr_char / char_count))
     print("By sentence: %d / %d, accuracy %.4f" % (corr_sent, len(valid_set), corr_sent / len(valid_set)))
 
 
 if __name__ == '__main__':
-    get_char_freq()
+    # get_ngram(Path("/Users/casorazitora/Desktop/Learn new stuff/大二下/人智导/输入法作业/语料库/sina_news.txt"),
+              # Path("course_pinyin_dict.json"), Path("sina_news.json"))
+    with opr("sina_news", 'json') as fi:
+        ngram_dict = json.load(fi)
 
+    validate(lambda pyl: parse_pinyin_by_ngram(pyl, ngram_dict), Path('.') / "my_output")
