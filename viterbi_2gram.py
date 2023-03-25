66a4c34797d06896d9ab57734bdf1044238a841d
陈庆之 <cqz21@mails.tsinghua.edu.cn>
2023-03-25 13:51:46 +0800

    viterbi implementation

--- viterbi_2gram.py
+++ viterbi_2gram.py
@@ -7,26 +7,21 @@ from typing import Optional
 begin_mark = '^'
 
 
-def get_prob_dict(corpus_file: Path, pinyin_file: Path, output_file: Path, smoothing_factor: float = 0.2):
+def get_ngram_freq(corpus: list[str], pinyin_dict: dict, verbose: bool = True):
     gram_dict = {}
     freq_dict = {}
 
-    with corpus_file.open(mode='r', encoding='utf-8') as fi:
-        corpus = [line.strip().replace(begin_mark, '') for line in fi.readlines()]
-    with pinyin_file.open(mode='r', encoding='utf-8') as fi:
-        pinyin_dict = json.load(fi)
-    print("loaded!")
-
     accepted_chars = set([char for group in pinyin_dict.values() for char in group])
-    char_count = len(accepted_chars)
 
     count = 0
     t = time.time()
     for sent in corpus:
+        sent = sent.strip()
         count += 1
         if count % 10000 == 0:
             newt = time.time()
-            print("%d / %d sentences parsed in %d seconds" % (count, len(corpus), newt - t))
+            if verbose:
+                print("%d / %d sentences parsed in %d seconds" % (count, len(corpus), newt - t))
         if len(sent) == 0:
             continue
         segments = []
@@ -50,14 +45,27 @@ def get_prob_dict(corpus_file: Path, pinyin_file: Path, output_file: Path, smoot
                     gram_dict[seg[i]] = {}
                 gram_dict[seg[i]][seg[i + 1]] = gram_dict[seg[i]].get(seg[i + 1], 0) + 1
 
-    print("start computing")
+    return freq_dict, gram_dict
+
+
+def compute_prob(freq_dict: dict, gram_dict: dict, pinyin_dict: dict, smoothing_factor: float, verbose: bool = True):
+    if verbose:
+        print("start computing")
     prob_dict = {}
     total_freq = sum(freq_dict.values())
+    accepted_chars = set([char for group in pinyin_dict.values() for char in group])
+    char_count = len(accepted_chars)
+
     for c in accepted_chars:
+        # if a character is not present in the corpus
+        # we set its frequency to 1 to avoid math error
         freq_dict[c] = freq_dict.get(c, 1) / total_freq
+
     for prev_char in accepted_chars:
         prob_dict[prev_char] = {}
         if prev_char not in gram_dict:
+            # if the previous character is not present in the corpus
+            # we use frequency on the whole corpus instead
             for c in accepted_chars:
                 prob_dict[prev_char][c] = log(char_count)
         else:
@@ -74,6 +82,24 @@ def get_prob_dict(corpus_file: Path, pinyin_file: Path, output_file: Path, smoot
             smoothing_factor * gram_dict[begin_mark].get(c, 0) / total +
             (1 - smoothing_factor) * freq_dict[c]
         )
+
+    return prob_dict
+
+
+def get_prob_dict_core(corpus: list[str], pinyin_dict: dict, smoothing_factor: float = 0.2, verbose: bool = True):
+    freq_dict, gram_dict = get_ngram_freq(corpus, pinyin_dict, verbose)
+    prob_dict = compute_prob(freq_dict, gram_dict, pinyin_dict, smoothing_factor, verbose)
+    return prob_dict
+
+
+def get_prob_dict(corpus_file: Path, pinyin_file: Path, output_file: Path, smoothing_factor: float = 0.2):
+    with corpus_file.open(mode='r', encoding='utf-8') as fi:
+        corpus = [line.strip().replace(begin_mark, '') for line in fi.readlines()]
+    with pinyin_file.open(mode='r', encoding='utf-8') as fi:
+        pinyin_dict = json.load(fi)
+    print("loaded!")
+
+    prob_dict = get_prob_dict_core(corpus, pinyin_dict, smoothing_factor)
 
     print("start printing")
     with output_file.open(mode='w', encoding='utf-8') as fo:
@@ -116,13 +142,15 @@ def demo():
         print(viterbi_2gram(py, prob_dict, pinyin_dict))
 
 
-def validate(predict_function, output: Optional[Path] = None):
+def validate(predict_function, output: Optional[Path] = None, verbose: bool = True):
     with open('validation/input.txt', mode='r', encoding='utf-8') as fi:
         valid_set = [line.strip() for line in fi]
     with open('validation/std_output.txt', mode='r', encoding='utf-8') as fi:
         std_output = [line.strip() for line in fi]
 
-    print(len(valid_set))
+    if verbose:
+        print(len(valid_set))
+    t0 = time.time()
     char_count = sum([len(t) for t in std_output])
     corr_char = 0
     corr_sent = 0
@@ -145,21 +173,38 @@ def validate(predict_function, output: Optional[Path] = None):
                 flag = False
         if flag:
             corr_sent += 1
+    elapsed = time.time() - t0
 
     if fout is not None:
         fout.close()
-    print("By character: %d / %d, accuracy %.4f" % (corr_char, char_count, corr_char / char_count))
-    print("By sentence: %d / %d, accuracy %.4f" % (corr_sent, len(valid_set), corr_sent / len(valid_set)))
+    if verbose:
+        print("By character: %d / %d, accuracy %.4f" % (corr_char, char_count, corr_char / char_count))
+        print("By sentence: %d / %d, accuracy %.4f" % (corr_sent, len(valid_set), corr_sent / len(valid_set)))
+        print("Validation time: %d secs, avg %.2f sentences per second" % (int(elapsed), len(valid_set) / elapsed))
+    return corr_char / char_count, corr_sent / len(valid_set)
+
+
+def auto_learn(corpus: list[str], pinyin_dict: dict, init_fa: float = 0.5, init_step: float = 0.5):
+    fa = init_fa
+    step = init_step
+    pd = get_prob_dict_core(corpus, pinyin_dict, fa, False)
+    c, _ = validate(lambda t: viterbi_2gram(t.strip().split(), pd, pinyin_dict), None, False)
+
+
+def train_and_test_sina_news(fa: float):
+    corpus = Path('corpus/sina_news.txt').open(mode='r', encoding='utf-8').readlines()
+    pinyin_dict = json.load(Path('pinyin_to_hanzi.json').open(mode='r', encoding='utf-8'))
+    prob_dict = get_prob_dict_core(corpus, pinyin_dict, fa, True)
+    with open('pinyin_to_hanzi.json', mode='r', encoding='utf-8') as fi:
+        pinyin_dict = json.load(fi)
+    validate(lambda t: viterbi_2gram(t.strip().split(), prob_dict, pinyin_dict), Path("my_output.txt"))
+
+
+def train_sina_news(fa: float):
+    get_prob_dict(Path('corpus/sina_news.txt'), Path('pinyin_to_hanzi.json'), Path('sina_news-%.4f.json' % fa), fa)
 
 
 if __name__ == '__main__':
-    get_prob_dict(Path('corpus/weibo.txt'), Path('outer.json'), Path('weibo_outer.json'))
-    # demo()
-    with open('weibo_outer.json', mode='r', encoding='utf-8') as fi:
-        prob_dict = json.load(fi)
-    # with open('pinyin_to_hanzi.json', mode='r', encoding='utf-8') as fi:
-    with open('outer.json', mode='r', encoding='utf-8') as fi:
-        pinyin_dict = json.load(fi)
-    print("loaded!")
-    validate(lambda t: viterbi_2gram(t.strip().split(), prob_dict, pinyin_dict), Path("my_output.txt"))
+    pass
+
 
