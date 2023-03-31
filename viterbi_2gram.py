a25ad99841a1a075d12d6527857e4decf57834b8
陈庆之 <cqz21@mails.tsinghua.edu.cn>
2023-03-31 22:33:42 +0800

    3gram

--- viterbi_2gram.py
+++ viterbi_2gram.py
@@ -78,7 +78,7 @@ def get_ngram(corpus: list[list[str]], gram_count: int, verbose: bool = True):
                 key = seg[i: i + gram_count - 1]
                 if key not in gram_dict:
                     gram_dict[key] = MyDict()
-                gram_dict[key].incr([seg[i + 1]])
+                gram_dict[key].incr(seg[i + gram_count - 1])
 
     return gram_dict
 
@@ -97,11 +97,10 @@ def compute_loss(freq_dict: dict, gram_dict: dict, accepted_chars: set, smoothin
     """
     if verbose:
         print("start computing loss")
-    begin_mark = (len(list(gram_dict.keys())[0]) - 1) * begin_char
-    default_mark = (len(list(gram_dict.keys())[0]) - 1) * default_char
+    begin_mark = (len(list(gram_dict.keys())[0])) * begin_char
+    default_mark = (len(list(gram_dict.keys())[0])) * default_char
     loss_dict = {}
     total_freq = sum(freq_dict.values())
-    char_count = len(accepted_chars)
 
     for c in accepted_chars:
         # if a character is not present in the corpus
@@ -110,18 +109,12 @@ def compute_loss(freq_dict: dict, gram_dict: dict, accepted_chars: set, smoothin
 
     for query_str in gram_dict:
         loss_dict[query_str] = {}
-        if query_str not in gram_dict:
-            # if the previous character is not present in the corpus
-            # we use frequency on the whole corpus instead
-            for c in accepted_chars:
-                loss_dict[query_str][c] = log(char_count)
-        else:
-            total = sum(gram_dict[query_str].values())
-            for c in accepted_chars:
-                loss_dict[query_str][c] = - log(
-                    smoothing_factor * gram_dict[query_str].get(c, 0) / total +
-                    (1 - smoothing_factor) * freq_dict[c]
-                )
+        total = sum(gram_dict[query_str].values())
+        for c in gram_dict[query_str]:
+            loss_dict[query_str][c] = - log(
+                smoothing_factor * gram_dict[query_str].get(c, 0) / total +
+                (1 - smoothing_factor) * freq_dict[c]
+            )
     loss_dict[begin_mark] = {}
     total = sum(gram_dict[begin_mark].values())
     for c in accepted_chars:
@@ -131,12 +124,15 @@ def compute_loss(freq_dict: dict, gram_dict: dict, accepted_chars: set, smoothin
         )
 
     # add default losses
+    for key in freq_dict:
+        freq_dict[key] = -log(freq_dict[key])
     loss_dict[default_mark] = freq_dict
+    print("done computing!")
     return loss_dict
 
 
-def get_loss_dict_core(corpus: list[list[str]], accepted_chars: set, gram_count: int,
-                       smoothing_factor: float, freq_dict: dict = None, verbose: bool = True):
+def get_loss_dict(corpus: list[list[str]], accepted_chars: set, gram_count: int,
+                  smoothing_factor: float, freq_dict: dict = None, verbose: bool = True):
     if freq_dict is None:
         freq_dict = get_freq(corpus, verbose)
     gram_dict = get_ngram(corpus, gram_count, verbose)
@@ -144,30 +140,15 @@ def get_loss_dict_core(corpus: list[list[str]], accepted_chars: set, gram_count:
     return loss_dict
 
 
-def get_loss_dict(corpus_file: Path, pinyin_file: Path, output_file: Path,
-                  gram_count: int, smoothing_factor: float = 0.2):
-    with corpus_file.open(mode='r', encoding='utf-8') as fi:
-        corpus = [line.strip().replace(begin_char, '') for line in fi.readlines()]
-    with pinyin_file.open(mode='r', encoding='utf-8') as fi:
-        pinyin_dict = json.load(fi)
-    print("loaded!")
-
-    accepted_chars = set([char for group in pinyin_dict.values() for char in group])
-    corpus = wash_corpus(corpus, accepted_chars)
-    loss_dict = get_loss_dict_core(corpus, accepted_chars, gram_count, smoothing_factor)
-
-    print("start printing")
-    with output_file.open(mode='w', encoding='utf-8') as fo:
-        json.dump(loss_dict, fo)
-
-
-def viterbi_ngram(pinyin: list[str], loss_dict: dict, pinyin_dict: dict, gram_count: int):
+def viterbi_ngram(pinyin: list[str], loss_dict: dict, pinyin_dict: dict, gram_count: int, smoothing_factor: float):
     # Each entry in a layer is of form added_char: (min_loss, prefix_str).
     # Both store what you would expect them to.
     begin_mark = begin_char * (gram_count - 1)
-    prev_layer = {begin_mark: (0, '')}
+    prev_layer = {begin_mark: (0, begin_char * (gram_count - 1))}
     curr_layer = {}
     default_mark = default_char * (gram_count - 1)
+    factor_loss = -log(1 - smoothing_factor)
+
     for syl in pinyin:
         if syl not in pinyin_dict:
             # panic: output an underscore and restart the sentence
@@ -178,23 +159,22 @@ def viterbi_ngram(pinyin: list[str], loss_dict: dict, pinyin_dict: dict, gram_co
                 prev_best_key = ''
                 min_loss = math.inf
                 for key in prev_layer:
-                    ngram_prefix = prev_layer[key][1][1 - gram_count:] + key
-                    loss = prev_layer[key][0] + loss_dict.get(ngram_prefix, loss_dict[default_mark])[char]
+                    ngram_prefix = prev_layer[key][1][1 - gram_count:]
+                    loss = prev_layer[key][0] + loss_dict.get(ngram_prefix, loss_dict[default_mark]).get(
+                        char, factor_loss + loss_dict[default_mark][char])
                     if loss < min_loss:
                         min_loss = loss
                         prev_best_key = key
-
-                curr_layer[char] = (prev_layer[prev_best_key][0] + loss_dict[prev_best_key][char],
-                                    prev_layer[prev_best_key][1] + char)
+                curr_layer[char] = (min_loss, prev_layer[prev_best_key][1] + char)
         prev_layer = curr_layer
         curr_layer = {}
 
-    return prev_layer[min(prev_layer, key=lambda k: prev_layer[k][0])][1]
+    return prev_layer[min(prev_layer, key=lambda k: prev_layer[k][0])][1][gram_count - 1:]
 
 
 def demo():
     print(time.time())
-    with open('weibo.json', mode='r', encoding='utf-8') as fi:
+    with open('weibo_loss.json', mode='r', encoding='utf-8') as fi:
         loss_dict = json.load(fi)
     print(time.time())
     with open('pinyin_to_hanzi.json', mode='r', encoding='utf-8') as fi:
@@ -204,7 +184,7 @@ def demo():
     while True:
         py = input()
         py = py.strip().split()
-        print(viterbi_ngram(py, loss_dict, pinyin_dict, 2))
+        print(viterbi_ngram(py, loss_dict, pinyin_dict, 2, 0.99))
 
 
 def validate(predict_function, output: Optional[Path] = None, verbose: bool = True):
@@ -249,5 +229,30 @@ def validate(predict_function, output: Optional[Path] = None, verbose: bool = Tr
     return corr_char / char_count, corr_sent / len(valid_set)
 
 
+def demo_validate():
+    with open('weibo_loss.json', mode='r', encoding='utf-8') as fi:
+        loss_dict = json.load(fi)
+    with open('pinyin_to_hanzi.json', mode='r', encoding='utf-8') as fi:
+        pinyin_dict = json.load(fi)
+    print('file loaded')
+
+    validate(lambda t: viterbi_ngram(t.split(), loss_dict, pinyin_dict, 3, 0.99), Path('3gram_output.txt'))
+
+
 if __name__ == '__main__':
-    pass
+    demo_validate()
+    """demo()
+    with open('corpus/weibo.txt', mode='r', encoding='utf-8') as fi:
+        corpus = [line.strip().replace(begin_char, '') for line in fi.readlines()]
+    with open('pinyin_to_hanzi.json', mode='r', encoding='utf-8') as fi:
+        pinyin_dict = json.load(fi)
+    print("loaded!")
+
+    accepted_chars = set([char for group in pinyin_dict.values() for char in group])
+    corpus = wash_corpus(corpus, accepted_chars)
+    loss_dict = get_loss_dict(corpus, accepted_chars, 3, 0.99)
+
+    with open('weibo_loss.json', mode='w', encoding='utf-8') as fo:
+        json.dump(loss_dict, fo)"""
+
+
